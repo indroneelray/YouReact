d2585e1bcb3233840939fc76bdbcb66e67e4aaca
Indroneel Ray <indroneelray@mymacbookproisnotforeveryonetoview.local>
2018-04-02 13:06:24 +0300

    nn

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,6 +1,6 @@
 //since it doesnt record any user action, we can make it a plain functional component
 
-improt React from 'react'
+import React from 'react'
 
 const VideoList = () =>{
         <ul className="col-md-4 list-group">
